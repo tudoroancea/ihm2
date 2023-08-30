598a9b04b6d70a5ddbd2e9c9de2090c960a47c3c
Ted <oancea.tudor@icloud.com>
2023-08-30 13:47:45 +0200

    updated markers

--- src/ihm2_python/ihm2_python/mesh_publisher.py
+++ src/ihm2_python/ihm2_python/mesh_publisher.py
@@ -1,18 +1,13 @@
-from matplotlib import scale
 import numpy as np
 import rclpy
-# import track_database as tdb
 from diagnostic_msgs.msg import *
 from geometry_msgs.msg import *
 from rclpy.node import Node
-from rclpy.publisher import Publisher
-from rclpy.qos import *
-# from rclpy.subscription import Subscription
-from rclpy.time import Time
 from std_msgs.msg import ColorRGBA, Header
-# from tf2_geometry_msgs import from_msg_msg
 from scipy.spatial.transform import *
 from visualization_msgs.msg import Marker, MarkerArray
+from track_database import Track
+
 
 def rpy_to_quaternion(roll: float, pitch: float, yaw: float):
     t0 = np.cos(yaw * 0.5)
@@ -28,6 +23,8 @@ def rpy_to_quaternion(roll: float, pitch: float, yaw: float):
         t0 * t2 * t5 + t1 * t3 * t4,  # y
         t1 * t2 * t4 - t0 * t3 * t5,  # z
     ]
+
+
 marker_colors = {
     "red": ColorRGBA(r=1.0, g=0.0, b=0.0, a=1.0),
     "green": ColorRGBA(r=0.0, g=1.0, b=0.0, a=1.0),
@@ -47,69 +44,98 @@ marker_colors = {
     "dark_gray": ColorRGBA(r=0.25, g=0.25, b=0.25, a=1.0),
 }
 
-class MeshPublisherNode(Node):
+
+def car_marker(X, Y, phi):
+    qw, qx, qy, qz = rpy_to_quaternion(0, 0, np.pi / 2 + phi)
+    q = Quaternion(w=qw, x=qx, y=qy, z=qz)
+    return Marker(
+        header=Header(frame_id="world"),
+        ns="mesh",
+        type=Marker.MESH_RESOURCE,
+        action=Marker.MODIFY,
+        mesh_resource="https://github.com/tudoroancea/ihm2/releases/download/lego-lrt4/lego-lrt4.stl",
+        scale=Vector3(x=0.03, y=0.03, z=0.03),
+        color=ColorRGBA(r=1.0, g=1.0, b=1.0, a=1.0),
+        pose=Pose(
+            position=Point(
+                x=X,
+                y=Y,
+                z=0.0,
+            ),
+            orientation=q,
+        ),
+    )
+
+
+cone_marker = lambda id, X, Y, color, small: Marker(
+    header=Header(frame_id="world"),
+    ns="cones",
+    id=id,
+    type=Marker.ARROW,
+    action=Marker.MODIFY,
+    scale=Vector3(x=0.0, y=0.228, z=0.325)
+    if small
+    else Vector3(x=0.0, y=0.285, z=0.505),
+    color=marker_colors[color],
+    points=[
+        Point(
+            x=X,
+            y=Y,
+            z=0.0,
+        ),
+        Point(
+            x=X,
+            y=Y,
+            z=0.325 if small else 0.505,
+        ),
+    ],
+)
+class MeshPublisher(Node):
     def __init__(self):
-        super().__init__('mesh_publisher')
+        super().__init__("mesh_publisher")
         self.timer = self.create_timer(1.0, self.timer_callback)
-        self.publisher = self.create_publisher(MarkerArray, 'mesh', 10)
-    
-    def timer_callback(self):
-        self.get_logger().info('publishing mesh')
-        cone_marker = lambda id, X, Y, color : Marker(
-                header=Header(frame_id="world"),
-                ns="cones",
-                id=id,
-                type=Marker.ARROW,
-                action=Marker.MODIFY,
-                scale=Vector3(x=0.0, y=0.228, z=0.325),
-                color=marker_colors[color],
-                points=[
-                    Point(
-                        x=X,
-                        y=Y,
-                        z=0.0,
-                    ),
-                    Point(
-                        x=X,
-                        y=Y,
-                        z=0.325,
-                    ),
-                ],
+        self.publisher = self.create_publisher(MarkerArray, "mesh", 10)
+        self.track = Track("fsds_competition_2")
+        self.map_markers = [
+            cone_marker(i, X, Y, "orange", False)
+            for i, (X, Y) in enumerate(self.track.big_orange_cones)
+        ] + [
+            cone_marker(
+                self.track.big_orange_cones.shape[0] + i, X, Y, "blue", True
             )
-        qw, qx, qy, qz = rpy_to_quaternion(0, 0, np.pi)        
-        q = Quaternion(w=qw, x=qx, y=qy, z=qz)
+            for i, (X, Y) in enumerate(self.track.blue_cones)
+        ] + [
+            cone_marker(
+                self.track.big_orange_cones.shape[0]
+                + self.track.blue_cones.shape[0]
+                + i,
+                X,
+                Y,
+                "yellow",
+                True,
+            )
+            for i, (X, Y) in enumerate(self.track.yellow_cones)
+        ]
+
+    def timer_callback(self):
+        t0 = self.get_clock().now()
         self.publisher.publish(
             MarkerArray(
                 markers=[
-                    cone_marker(0, 0.0, 0.0, "blue"),
-                    cone_marker(1, 1.0, 0.0, "yellow"),
-                    Marker(
-                        header=Header(frame_id="world"),
-                        ns="mesh",
-                        type=Marker.MESH_RESOURCE,
-                        action=Marker.MODIFY,
-                        mesh_resource="https://github.com/tudoroancea/ihm2/releases/download/lego-lrt4/lego-lrt4.stl",
-                        scale=Vector3(x=0.03, y=0.03, z=0.03),
-                        color=ColorRGBA(r=1.0, g=1.0, b=1.0, a=1.0),
-                        pose=Pose(
-                            position=Point(
-                                x=2.0,
-                                y=0.0,
-                                z=0.0,
-                            ),
-                            orientation=q,
-                        ),
-                    ),
-                ]
+                    car_marker(0.0, 0.0, np.pi / 2),
+                ] + self.map_markers
             )
         )
+        t1 = self.get_clock().now()
+        self.get_logger().info(f"published mesh in {(t1 - t0).nanoseconds * 1e-6} ms")
+
 
 def main():
     rclpy.init()
-    node = MeshPublisherNode()
+    node = MeshPublisher()
     rclpy.spin(node)
     rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- src/ihm2_python/setup.py
+++ src/ihm2_python/setup.py
@@ -1,24 +1,21 @@
 from setuptools import setup
 
-package_name = 'ihm2_python'
+package_name = "ihm2_python"
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version="0.0.0",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='tudoroancea',
-    maintainer_email='oancea.tudor@icloud.com',
-    license='MIT',
+    maintainer="tudoroancea",
+    maintainer_email="oancea.tudor@icloud.com",
+    license="MIT",
     entry_points={
-        'console_scripts': [
-            'mesh_publisher = ihm2_python.mesh_publisher:main'
-        ],
+        "console_scripts": ["mesh_publisher = ihm2_python.mesh_publisher:main"],
     },
 )
